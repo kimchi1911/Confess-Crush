689bf162022b13107e0f9721f2dbd32a9845f441
kimchi1911 <102298362+kimchi1911@users.noreply.github.com>
2022-03-24 19:15:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "helo bbi",
+    introTitle: ' bbi thien',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: 'ehee🐸',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    btnYes: 'thich lamm<3',
+    btnNo: 'khongg nhă>:(',
+    question: 'trên thế giới có hơn 7 tỉ người mà sao anh lai yêu em:(?',
+    btnReply: 'xin tra loi em',
+    reply: 'iu thi iu kim chi ma khong iu thi iu chi=)))))',
+    mess: 'biet ma:))) oke nhe iu anhh<3',
+    messDesc: ' anh to^`i vai nhma iu anhh ✨',
+    btnAccept: 'okii',
+    messLink: 'https://www.facebook.com/kymchie.1911' 
 }
